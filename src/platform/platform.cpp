c0698c7c7ce2e904f49a79a54e2bd6ed8af5c4d1
ayoubbargach <bargachayoub@gmail.com>
2018-10-02 19:02:22 +0200

    added platform component

---
+++ src/platform/platform.cpp
@@ -0,0 +1,8 @@
+/**
+    CS-11 Asn 2
+    platform.cpp
+    Purpose: This implementation control the initialisation of all components and their simulation.
+
+    @author Ayoub Bargach
+    @version 1.0
+*/
---
+++ src/platform/platform.hpp
@@ -0,0 +1,32 @@
+/**
+    CS-11 Asn 2
+    platform.hpp
+    Purpose: The platform is the central core of our implemention.
+
+    @author Ayoub Bargach
+    @version 1.0
+*/
+
+
+#ifndef PLATFORM_HPP
+#define PLATFORM_HPP
+
+#include <map>
+#include "component/Component.hpp"
+#include "format.hpp"
+
+
+class Platform {
+private:
+    int steps; // Input by the user
+    bool verbose; // Input by the user
+    FileManipulator myFile;
+    std::map<std::string, Component> components; // LABEL -> component
+public:
+    Platform(std::string generalConfigFile, int steps, bool verbose);
+    void bind(); // Perform the binding of all the components
+    void simulate(); // Simulate all the components
+};
+
+
+#endif
