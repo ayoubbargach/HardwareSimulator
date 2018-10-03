5add75ed3046d34a6bf75fdb7d4c51b759945bc7
ayoubbargach <bargachayoub@gmail.com>
2018-10-03 13:35:13 +0200

    Platform contructor OK

--- src/platform/platform.cpp
+++ src/platform/platform.cpp
@@ -27,17 +27,16 @@ Platform::Platform(std::string generalConfigFile, int steps, bool verbose)
 
 
         if (typeComponent == "CPU") {
-            Component * tempComponent = new Cpu( Config(line), verbose) );
-            components.insert(std::pair<std::string,Component>(labelComponent, tempComponent));
+            components.insert(std::pair<std::string,Component *>(labelComponent, new Cpu( Config(line), verbose)));
         }
         else if (typeComponent == "BUS") {
-            components.insert(std::pair<std::string,Component>(labelComponent, &Bus( Config(line), verbose) ));
+            components.insert(std::pair<std::string,Component *>(labelComponent, new Bus( Config(line), verbose)));
         }
         else if (typeComponent == "MEMORY") {
-            components.insert(std::pair<std::string,Component>(labelComponent, &Memory( Config(line), verbose) ));
+            components.insert(std::pair<std::string,Component *>(labelComponent, new Memory( Config(line), verbose)));
         }
         else if (typeComponent == "DISPLAY") {
-            components.insert(std::pair<std::string,Component>(labelComponent, &Display( Config(line)) ));
+            components.insert(std::pair<std::string,Component *>(labelComponent, new Display( Config(line))));
         }
         else {
             std::cout << "A component have not been recognized, this issue may occur on failures. Please review your config platform file" << std::endl;
